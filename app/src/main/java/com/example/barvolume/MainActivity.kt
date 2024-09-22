d4c66052e6c7b95e4c72c539dfb1996323bf4756
MeydhiXploit <meydhimeydhi@gmail.com>
2024-09-22 09:57:34 +0800

    feat: calculated

--- app/src/main/java/com/example/barvolume/MainActivity.kt
+++ app/src/main/java/com/example/barvolume/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.barvolume
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnKeyListener, View.OnClickListener {
+    private lateinit var edtWidth:EditText
+    private lateinit var edtHeight:EditText
+    private lateinit var edtLength:EditText
+    private lateinit var btnCalculated:Button
+    private lateinit var tvResult:TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +27,26 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+            edtWidth = findViewById(R.id.edt_Width)
+            edtHeight = findViewById(R.id.edt_Height)
+            edtLength = findViewById(R.id.edt_Length)
+            btnCalculated = findViewById(R.id.btn_Calculated)
+            tvResult = findViewById(R.id.tv_Hasil)
+            btnCalculated.setOnClickListener(this)
     }
+
+    override fun onClick(view: View?) {
+        if(view?.id == R.id.btn_Calculated){
+            val inputLength = edtLength.text.toString().trim()
+            val inputWidth = edtWidth.text.toString().trim()
+            val inputHeight = edtHeight.text.toString().trim()
+            val volume = inputLength.toDouble() * inputWidth.toDouble() * inputHeight.toDouble()
+            tvResult.text = volume.toString()
+        }
+    }
+
+    override fun onKey(p0: View?, p1: Int, p2: KeyEvent?): Boolean {
+        TODO("Not yet implemented")
+    }
+
 }
--- app/src/main/java/com/example/barvolume/activity_adders.kt
+++ app/src/main/java/com/example/barvolume/activity_adders.kt
@@ -1,6 +1,9 @@
 package com.example.barvolume
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,22 @@ class activity_adders : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        //inisialiasi komponen
+        var edtTanggalLahir = findViewById<EditText>(R.id.edtTanggalLahir)
+        var btnKlik = findViewById<Button>(R.id.btnClick)
+        var tvHasil = findViewById<TextView>(R.id.tvResult)
+
+        btnKlik.setOnClickListener {
+            val input = edtTanggalLahir.text.toString()
+            val hasil = when(input.toInt()){
+                in 1946..1964 -> "Baby boomers"
+                in 1965..1980 -> "X"
+                in 1981..1995 -> "Millenial"
+                in 1996..2010 -> "Z"
+                else -> "Gak tau mass"
+            }
+            tvHasil.text = "Kamu generasi $hasil"
         }
     }
 }
